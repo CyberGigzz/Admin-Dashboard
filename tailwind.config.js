46a3bfcc2fc07ce69f6f5e4e10d72d7e622e2240
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-06-18 16:34:24 +0400

    Working on layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
-  
+
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // 1 / 4 column
+        '1-4': "1fr 4fr"
+      },
+      gridTemplateRows: {
+        // 1/4 row
+        '1-4': "1fr 4fr"
+      }
+    },
   },
   plugins: [],
-}
-
+};
