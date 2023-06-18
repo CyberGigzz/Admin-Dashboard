a3f6d47918ebe8bb62b3209d7e9b480d3411b3a5
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-06-18 20:35:42 +0400

    UI done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,22 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         // 1 / 4 column
-        '1-4': "1fr 4fr"
+        "1-4": "1fr 4fr",
+        custom: "7fr 3fr",
       },
       gridTemplateRows: {
         // 1/4 row
-        '1-4': "1fr 4fr"
+        "1-4": "1fr 4fr",
       },
       boxShadow: {
-        'header': '0px 2px 3px rgb(175, 175, 175, 0.5)',
+        header: "0px 2px 3px rgb(175, 175, 175, 0.5)",
       },
       backgroundColor: {
-        'custom-blue': 'rgba(226, 232, 240, 1)',
+        "custom-blue": "rgba(226, 232, 240, 1)",
       },
       maxWidth: {
-        '70': '70%',
-      }
+        65: "65%",
+      },
     },
   },
   plugins: [],
